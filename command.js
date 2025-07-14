9ec14d7fe999dd9f658a31a31d90ac9a8a738b03
Sukriti Waani <sukritiwaani2006@gmail.com>
2025-07-14 20:30:31 +0530

    Add message and interaction handlers with global /create slash command registration

--- command.js
+++ command.js
@@ -13,10 +13,11 @@ const commands = [
 
 // Create an instance of the REST API client
 // The version parameter specifies the Discord API version to use (v10 is the latest major version as of discord.js v14)
-const rest = new REST({ version: "10" }).setToken(
+const rest = new REST({ version: "10" })
+  .setToken
   // Set the bot token to authenticate the REST API requests
-   // {MY_TOKEN}
-);
+  // {MY_TOKEN}
+  ();
 
 // Immediately Invoked Async Function Expression (IIFE) to execute the registration logic
 (async () => {
@@ -26,12 +27,9 @@ const rest = new REST({ version: "10" }).setToken(
 
     // Register (or update) the slash commands globally for the given Application ID
     // Routes.applicationCommands registers commands globally, meaning they may take up to 1 hour to update
-    await rest.put(
-      Routes.applicationCommands("CLIENT_ID"), 
-      {
-        body: commands, // The array of commands to register
-      }
-    );
+    await rest.put(Routes.applicationCommands("CLIENT_ID"), {
+      body: commands, // The array of commands to register
+    });
 
     // Log a success message after commands are registered
     console.log("Successfully reloaded application (/) commands.");
